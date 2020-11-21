dfcd6229624363c57d56012180d4ec8b05eacabc
Suraj Singh <50519406+surajsly@users.noreply.github.com>
2020-11-21 09:29:29 +0530

    node first app -- fs node core module

---
+++ Node-first-app/first-app.js
@@ -0,0 +1,6 @@
+// file system 
+// fs is node core module
+const fs =  require('fs')
+
+// writeFileSync create a file syntax:  writeFileSync(path,content)
+fs.writeFileSync('hello.txt',"hello from node JS");
