5bd2026682c7d0cadb8d0f599a3eaafbfe863140
Suraj Singh <50519406+surajsly@users.noreply.github.com>
2020-11-24 12:46:42 +0530

    force shut server

--- Node-first-server/app.js
+++ Node-first-server/app.js
@@ -3,14 +3,22 @@
 //importing core modules
 const http = require("http");
 
-// A request listener receives :
+
+
+// http.createServer method create a server  
+
+// A request listener receives  ( In this case the arrow funtion wriiten inside http.createServer )
 // ( 1 )  A request which is of type incoming message 
 // ( 2 )  A response object,
 
-function rqListner(req,res){
+const server = http.createServer((req,res)=>{
+	console.log(req);
+	// process.exit() forced shut down the server 
+	// process.exit();
+});
 
-}
-
-// this method create a server and take a request listener 
-
-http.createServer(rqListner);
+// Listen take optional argument  port, host name 
+// this keeps on litening for upcomming request
+// here server run on 
+// http://localhost:3000/
+server.listen(3000);
