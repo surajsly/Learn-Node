08bb9e37702be65e58d54b164b9435ac7d068bb1
surajsly <50519406+surajsly@users.noreply.github.com>
2020-11-30 10:00:20 +0530

    parsing response body

--- Node-first-server/app.js
+++ Node-first-server/app.js
@@ -30,8 +30,25 @@ const server = http.createServer((req,res)=>{
 	}
 
 	if (url === '/message' && method === 'POST'){
-		// creating and saving file
-		fs.writeFileSync('message.txt', "DUMMY")
+
+		const body = [];
+
+		// req.on is a event listener which will run for every chunk of data
+		req.on('data', (chunk) => {
+			console.log(chunk)
+			body.push(chunk);
+		});
+
+		// req.on('end') will run when all chunked recived 
+		req.on('end', ()=>{
+			const parsedBody = Buffer.concat(body).toString();
+			console.log(parsedBody);
+			const message = parsedBody.split('=')[1]
+
+			// creating and saving file
+			fs.writeFileSync('message.txt', message )
+
+		})
 		
 		// redirecting to "/" 
 		// status code 302 means discribe redirection 
@@ -43,13 +60,11 @@ const server = http.createServer((req,res)=>{
 	
 	// res.setHeader set certain header for response
 	res.setHeader('Content-Type',"text/html");
-
 	// res.write allow you to write response
 	res.write('<html>');
 	res.write('<head> <title> My first Page </title> </head>');
 	res.write('<body> <h1> hello from my node.js server </h1> </body>');
 	res.write('</html>');
-
 	// res.end signifies response writing . You cannot use res.write beyond this as it will result in error 
 	res.end();
 
